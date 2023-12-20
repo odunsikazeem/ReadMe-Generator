4fec958eceba9b1cfb9ac3f95b74383377c1db9a
odunsikazeem <145531096+odunsikazeem@users.noreply.github.com>
2023-12-20 12:57:02 +0000

    added index.js & markdown.js codes

--- starter/index.js
+++ starter/index.js
@@ -4,16 +4,68 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = [ {
+    type: "input",
+    name: "title",
+    message: "Please name your Project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe the purpose and functionality of this project.",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "List some features about this application.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "State the languages or technologies associated with this project.",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Write your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributors. (Use GitHub usernames)",
+    default: "",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Provide walkthrough of required tests if applicable.",
+  },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README.md File...");
+        writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+      });
 
 }
 
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
-}
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#Usage)
+  * [Features](#features)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## Features
+  ${data.features}
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  ${data.test}
+  ## Questions
+  visit [github/${data.creator}](https://github.com/${data.creator}).
+  Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect). 
+  `;
+  }
 
 module.exports = generateMarkdown;
